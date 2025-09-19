c7429e7ceb42a66db146ba8151e4e4df0b4d6d60
devinfo <devinformationproject@gmail.com>
2025-09-19 17:43:50 +0200

    creating the backdrop image for the films

--- js/script.js
+++ js/script.js
@@ -84,9 +84,11 @@ function hideSpinner() {
 // Display Movie Details
 async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
-  console.log(movieId);
 
   const movie = await fetchAPIData(`movie/${movieId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
 
   const div = document.createElement('div');
 
@@ -135,11 +137,34 @@ async function displayMovieDetails() {
             <li><span class="text-secondary">Status:</span> ${movie.status}</li>
           </ul>
           <h4>Production Companies</h4>
-          <div class="list-group">Company 1, Company 2, Company 3</div>
+          <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
         </div>
   `;
 
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display Backdrop on Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.3';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // Fetch data from TMDB API
